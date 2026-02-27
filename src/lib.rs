6fbff5e8ac76a9a0c7d7aa19ecd1ffe730c7e861
zaarov <joao_rosa@tutamail.com>
2026-02-27 18:29:27 -0300

    prevent dll load error

--- src/lib.rs
+++ src/lib.rs
@@ -13,21 +13,27 @@ const DLL_PROCESS_ATTACH: u32 = 1;
 const DLL_PROCESS_DETACH: u32 = 0;
 
 #[unsafe(no_mangle)]
+#[allow(unused)]
 pub extern "system" fn DllMain(hinstDLL: usize, dwReason: u32, _lpReserved: *mut usize) -> i32 {
     match dwReason {
         DLL_PROCESS_ATTACH => unsafe {
-            if let Err(_) = init_proxy(hinstDLL) {
-                return 1;
-            }
+            let path: String = match init_proxy(hinstDLL) {
+                Ok(p) => p,
+                Err(e) => panic!("Could not proxy dll: {e}"),
+            };
 
-            thread::spawn(|| {
-                thread::sleep(Duration::from_secs(5));
+            let main_thread: thread::JoinHandle<()> = thread::spawn(|| {
+                thread::sleep(Duration::from_secs(3));
                 let _ = apply();
             });
 
             1
         },
-        DLL_PROCESS_DETACH => 1,
+        
+        DLL_PROCESS_DETACH => {
+            1
+        },
+        
         _ => 0,
     }
 }
